2246b42ab5c3960c7d4397c32f62174c618f10fb
Insurmountable-dev <264776215+Insurmountable-dev@users.noreply.github.com>
2026-07-22 14:39:05 +0300

    Display Farmers

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <windows.h>
+
+
+// Data
 
 // Number Of Farmers
 #define SIZE 6
-
 
 int farmerNumbers[SIZE] = {101, 102, 103, 104, 105, 106};
 
@@ -49,14 +54,107 @@ char paymentStatus[SIZE][10] = {
 
 // Function Prototypes
 
-
+void displayFarmers();
 
 
 // Main Program
 
 int main () {
 
-    printf("Project Ready.........");
+// Variables
+    bool isTrue = true;
+    int choice = 0;
+    bool validChoice = false;
+    int userChoice = 0;
+
+
+    do{
+
+        printf("\n===== FARMERS PRODUCE MANAGEMENT SYSTEM =====\n");
+        printf("\n1.Display Farmer Records\n");
+        printf("\n5.Exit\n");
+
+
+        // Verify The Choice
+        while(!validChoice) {
+
+            printf("\nWhat Would You Like To Do Today ? ");
+            userChoice = scanf("%d",&choice);
+
+
+            if(userChoice == 1){
+
+                if(choice > 0  && choice <= 5) {
+                    validChoice = true; //Valid User Input
+
+                }else{
+                    
+                    printf("\nInvalid Range Should Be  Between 1 and 5.......\n");
+                }
+                
+
+            }else{
+
+                printf("\nInvalid Input Kindly! Insert A Number.......\n");
+
+                    //Clearing Buffer
+                while(getchar() != '\n'); // Removes Bad Input
+
+            }
+
+
+
+        };
+
+
+
+      //DECIDING WHICH OPTION TO RUN BASED ON USERS CHOICE
+
+        switch(choice) {
+            case 1:
+                printf("\nFetching Data Offline\n");
+                Sleep(900);
+                displayFarmers();
+                break;
+
+            case 5:
+                 isTrue = false;
+                 break;
+
+        }
+
+
+        validChoice = false;
+
+
+    }while(isTrue);
+
 
     return 0;
 }
+
+
+
+
+
+// Function Components
+
+
+// display Farmers
+void displayFarmers(){
+
+    for(int i = 0; i < SIZE; i++) {
+
+        printf("\nFarmer Number : %d\n", farmerNumbers[i]);
+        printf("Farmer Name   : %s\n", farmerNames[i]);
+        printf("Produce Type  : %s\n", produceTypes[i]);
+        printf("Quantity      : %d\n", quantities[i]);
+        printf("Price/Unit    : %d\n", pricesPerUnit[i]);
+        printf("Payment Status: %s\n", paymentStatus[i]);
+        printf("--------------------------------\n");
+
+        Sleep(900);//Delay
+    }
+    
+
+};
